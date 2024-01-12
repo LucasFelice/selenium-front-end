70de87883abcaaeed649305f3704d8cf2f5f1a00
Lucas de Felice <lucasfelice92@gmail.com>
2024-01-12 13:52:36 -0300

    Subindo alterações

---
+++ target/classes/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $br.com.lucasdefelice.projetoSelenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
