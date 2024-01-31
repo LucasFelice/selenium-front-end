21d24639cca5cb1ce921a1c63496c4bd380c377e
Lucas de Felice <lucasfelice92@gmail.com>
2024-01-31 07:57:18 -0300

    subindo atualizações no arquivo baseTest.

--- src/test/java/core/baseTest.java
+++ src/test/java/core/baseTest.java
@@ -1,24 +1,13 @@
 package core;
 
-import org.apache.commons.io.FileUtils;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.rules.TestName;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import page.*;
 
-import java.io.File;
-import java.io.IOException;
-import static core.driverFactory.getDriver;
 import static core.driverFactory.killDriver;
 
 public class baseTest {
-
-    @Rule
-    public TestName testName = new TestName();
 
     public WebDriver driver;
     public DSL dsl;
@@ -30,7 +19,11 @@ public class baseTest {
     public canaisDeAtendimentoPage canaisAtendimento;
     public falarComUnimedPage falarComUnimed;
 
-    @Before
+    public static WebDriver getDriver() {
+        return driverFactory.getDriver();
+    }
+
+    @BeforeEach
     public void testInicializar() {
         dsl = new DSL();
         menu = new falarComUnimedPage();
@@ -46,28 +39,10 @@ public class baseTest {
 
     }
 
-    @After
-    public void testFinalizar() throws IOException {
-        TakesScreenshot ss = (TakesScreenshot) getDriver();
-        File arquivo =ss.getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(arquivo, new File("target" + File.separator +
-                "screenshot" + File.separator + testName.getMethodName() + ".jpg"));
-
-        if(propriedades.FECHAR_BROWSER) {
-            killDriver();
+    @AfterEach
+    public void testFinalizar()  {
+            if (propriedades.FECHAR_BROWSER) {
+                killDriver();
+            }
         }
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
 }
