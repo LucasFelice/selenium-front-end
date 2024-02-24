1c647151f7a3a5a37284a1b1a61db4b6c50ac3a3
Lucas de Felice <lucasfelice92@gmail.com>
2024-02-24 07:45:12 -0300

    subindo alterações no arquivo resultadoBuscaPage

--- src/test/java/page/resultadoBuscaPage.java
+++ src/test/java/page/resultadoBuscaPage.java
@@ -5,38 +5,39 @@ import maps.resultadoBuscaPageMaps;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
-import static core.driverFactory.getDriver;
+
+import static core.driverFactory.driver;
 
 public class resultadoBuscaPage extends basePage {
 
     resultadoBuscaPageMaps resultadoBusca = new resultadoBuscaPageMaps();
 
     public void perfilDoPrestadorVisivel() throws InterruptedException{
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(30));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
         wait.until(ExpectedConditions.presenceOfElementLocated(
                 resultadoBusca.btnPerfilDoPrestadorVisivel));
     }
 
     public String resultadoEncontradoDaSuaBusca() throws InterruptedException{
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         return obterTexto(resultadoBusca.txtResultadoEncontradoDaSuaBusca);
     }
 
     public String resultadoEncontradoDaSuaBuscaRefinida() throws InterruptedException{
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         return obterTexto(resultadoBusca.txtResultadoEncontradoDaSuaBuscaRefinida);
     }
 
     public void setEstado(String texto) {
         clicarBotao(resultadoBusca.btnEstado);
         escrever(resultadoBusca.inputEstado, texto);
-        simularTeclaEnter(resultadoBusca.seletorEstado);
+        simularTeclaTab(resultadoBusca.seletorEstado);
     }
 
     public void setCidade(String texto) {
         clicarBotao(resultadoBusca.btnCidade);
         escrever(resultadoBusca.inputCidade, texto);
-        simularTeclaEnter(resultadoBusca.seletorCidade);
+        simularTeclaTab(resultadoBusca.seletorCidade);
     }
 
     public void filtar() {
