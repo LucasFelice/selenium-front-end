eadcfbf23f334e55ea48f58f81083caf26e2b8ad
Lucas de Felice <lucasfelice92@gmail.com>
2024-02-24 07:41:56 -0300

    subindo alterações no arquivo MyExtension

--- src/test/java/Allure/MyExtension.java
+++ src/test/java/Allure/MyExtension.java
@@ -1,10 +1,8 @@
 package Allure;
 
-import org.apache.commons.lang3.exception.ExceptionContext;
 import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
-import java.util.Optional;
 
 public class MyExtension implements AfterTestExecutionCallback {
 
