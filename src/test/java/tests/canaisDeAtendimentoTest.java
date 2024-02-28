86282507646b645e27a34d79bbcf99ce22e5a268
Lucas de Felice <lucasfelice92@gmail.com>
2024-02-28 15:03:44 -0300

    subindo alterações no arquivo canaisDeAtendimentoTest

--- src/test/java/tests/canaisDeAtendimentoTest.java
+++ src/test/java/tests/canaisDeAtendimentoTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import core.baseTest;
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
 import com.github.javafaker.Faker;
@@ -17,6 +18,28 @@ public class canaisDeAtendimentoTest extends baseTest {
     String fakerEstado = faker.address().state();
     String fakerTelefone = faker.phoneNumber().cellPhone();
 
+    private JSONObject massaJson;
+
+    @Test
+    public void utilizandoJson() throws InterruptedException{
+        massaJson = null;
+        massaJson = getJsonDataObject("massa", "nao cliente");
+        home.canaisDeAtendimento();
+        Assertions.assertEquals(
+                "Como podemos te ajudar? ", canaisAtendimento.tituloComoPodemosTeAjudar());
+        canaisAtendimento.queroFalarComAUnimed();
+        Assertions.assertEquals(
+                "Quero falar com a Unimed", falarComUnimed.tituloQueroFalarComAUnimed());
+        falarComUnimed.nao();
+        falarComUnimed.setNome((String) massaJson.get("nome"));
+        falarComUnimed.setEmail((String) massaJson.get("email"));
+        falarComUnimed.setEstado((String) massaJson.get("estado"));
+        falarComUnimed.setCidade((String) massaJson.get("cidade"));
+        falarComUnimed.setTelefone((String) massaJson.get("telefone"));
+        falarComUnimed.setManifestacao((String) massaJson.get("manifestacao"));
+        falarComUnimed.setAssunto((String) massaJson.get("assunto"));
+        falarComUnimed.setMensagem((String) massaJson.get("mensagem"));
+    }
 
     @Test
     public void queroFalarComAUnimedNaoCliente() throws InterruptedException{
@@ -30,7 +53,7 @@ public class canaisDeAtendimentoTest extends baseTest {
         falarComUnimed.setNome(fakerFirstName + " " +fakerLastName);
         falarComUnimed.setEmail(fakerEmail);
         falarComUnimed.setEstado(fakerEstado);
-        falarComUnimed.setCidade();
+        falarComUnimed.setCidade("");
         falarComUnimed.setTelefone(fakerTelefone);
         falarComUnimed.setManifestacao("Elogio");
         falarComUnimed.setAssunto("Operadora Unimed");
@@ -50,7 +73,7 @@ public class canaisDeAtendimentoTest extends baseTest {
         falarComUnimed.setNome("");
         falarComUnimed.setEmail("");
         falarComUnimed.setEstado("");
-        falarComUnimed.setCidade();
+        falarComUnimed.setCidade("");
         falarComUnimed.setTelefone("");
         falarComUnimed.setManifestacao("");
         falarComUnimed.setMensagem("");
