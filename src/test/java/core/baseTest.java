3f2c26c58af4373e0f0315328a8c970cb57f2155
Lucas de Felice <lucasfelice92@gmail.com>
2024-02-28 15:02:23 -0300

    subindo alterações no arquivo baseTest

--- src/test/java/core/baseTest.java
+++ src/test/java/core/baseTest.java
@@ -1,11 +1,17 @@
 package core;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import page.*;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 public class baseTest extends driverFactory{
 
@@ -53,4 +59,36 @@ public class baseTest extends driverFactory{
                 driver.quit();
             }
         }
+
+
+
+    protected JSONObject getJsonDataObject(String jsonFileName, String dataGroup) {
+        JSONParser parser = new JSONParser();
+
+        jsonFileName = osAdaptFileName(jsonFileName);
+        JSONObject jsonDataObject = null;
+        try {
+            Object jsonFileObject = parser.parse(new FileReader(System.getProperty("user.dir") + File.separator + "src" +
+                    File.separator + "test" + File.separator + "java" + File.separator + "data" + File.separator + jsonFileName));
+            jsonDataObject = (JSONObject) jsonFileObject;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        assert jsonDataObject != null;
+        return (JSONObject) jsonDataObject.get(dataGroup);
+    }
+
+    private String osAdaptFileName(String baseFileName) {
+        if (System.getProperty("os.name").startsWith("Windows")) {
+            baseFileName += ".json";
+        }
+        return baseFileName;
+    }
+
+    protected String getJsonDataProperty(JSONObject jsonData, String jsonProperty){
+        return new String(jsonData.get(jsonProperty).
+                toString().getBytes(Charset.forName("ISO-8859-1")), Charset.forName("UTF-8"));
+    }
 }
